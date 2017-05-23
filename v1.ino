2f66d323c4284d29fe852290c67f4e9378ae20fc
Leonardo Oliveira <leosoliveira10@gmail.com>
2017-05-23 15:08:52 -0300

    Update v1.ino

--- v1.ino
+++ v1.ino
@@ -1,4 +1,5 @@
 #include <LiquidCrystal.h>
+#include <string.h> 
  
   LiquidCrystal lcd(0, 1, 5, 4, 3, 2); ///RS 0 .. EN  1.. D7 2.. D6 3.. D5 4.. D6 5
 
@@ -6,97 +7,115 @@ int bot = 9;
 int menu = 0; 
 int bomba1 = 12;
 int bomba2 = 11;
-int led1 = 10; 
-int led2 = 9;
-int led3 = 8; 
+int led_dis = A4; 
+//int led1 = 10; 
+//int led2 = 9;
+//int led3 = 8; 
+int lin1 = 13, lin2 = 12, lin3 = 11,  lin4 = 10; 
+int col1 = 9, col2 = 8, col3 = 7, col4 = 6; 
+char stri[16];
 
 void setup()
 {
+      //PINOS DO TECLADO MATRICIAL 
+      pinMode(lin1, OUTPUT); 
+      pinMode(lin2, OUTPUT); 
+      pinMode(lin3, OUTPUT); 
+      pinMode(lin4, OUTPUT); 
+      pinMode(col1, INPUT_PULLUP); //ligado resistor pullup interno para deixar a entrada do teclado matricial sempre em 1
+      pinMode(col2, INPUT_PULLUP); 
+      pinMode(col3, INPUT_PULLUP); 
+      pinMode(col4, INPUT_PULLUP); 
+      
+      //OUTROS PINOS 
       pinMode(bot, INPUT);
       pinMode(bomba1, OUTPUT);
       pinMode(bomba2, OUTPUT);
-      pinMode(led1, OUTPUT);
-      pinMode(led2, OUTPUT);
-      pinMode(led3, OUTPUT);AASSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS
+      pinMode(led_dis, OUTPUT); 
+      //pinMode(led1, OUTPUT);
+      //pinMode(led2, OUTPUT);
+      //pinMode(led3, OUTPUT);
       lcd.begin(16, 2);      
+
+      lcd.clear(); 
+}
+
+char ikey(){      //Função para retornar o caracter da tecla pressionada 
+
+    digitalWrite(lin1, LOW); 
+    digitalWrite(lin2, HIGH); 
+    digitalWrite(lin3, HIGH); 
+    digitalWrite(lin4, HIGH); 
+
+    if(digitalRead(col4) == 0) return 'a';
+    if(digitalRead(col3) == 0) return 'b';
+    if(digitalRead(col2) == 0) return 'c';
+    if(digitalRead(col1) == 0) return 'd';
+
+    digitalWrite(lin1, HIGH); 
+    digitalWrite(lin2, LOW); 
+    digitalWrite(lin3, HIGH); 
+    digitalWrite(lin4, HIGH); 
+
+    if(digitalRead(col4) == 0) return '3';
+    if(digitalRead(col3) == 0) return '6';
+    if(digitalRead(col2) == 0) return '9';
+    if(digitalRead(col1) == 0) return '#';
+
+    digitalWrite(lin1, HIGH); 
+    digitalWrite(lin2, HIGH); 
+    digitalWrite(lin3, LOW); 
+    digitalWrite(lin4, HIGH); 
+
+    if(digitalRead(col4) == 0) return '2';
+    if(digitalRead(col3) == 0) return '5';
+    if(digitalRead(col2) == 0) return '8';
+    if(digitalRead(col1) == 0) return '0';
+
+    digitalWrite(lin1, HIGH); 
+    digitalWrite(lin2, HIGH); 
+    digitalWrite(lin3, HIGH); 
+    digitalWrite(lin4, LOW); 
+
+    if(digitalRead(col4) == 0) return '1';
+    if(digitalRead(col3) == 0) return '4';
+    if(digitalRead(col2) == 0) return '7';
+    if(digitalRead(col1) == 0) return '*';
+
+   return ' ';
 }
  
 void loop()
-{
-    switch (menu){
-  
-            case 0:     lcd.clear();
-                        lcd.setCursor(0, 0);
-                        lcd.print("Maquina de suco");
-                        lcd.setCursor(4, 1);
-                        lcd.print("Aperte 1");
-                        digitalWrite(bomba1, LOW);
-                        digitalWrite(bomba2, LOW);
-                        break;
+{             
+
+              digitalWrite(led_dis, HIGH);
+              lcd.setCursor(0, 0);                //Menu principal 
+              lcd.print("Maquina de suco");
+              lcd.setCursor(0, 1);
+              lcd.print("Escolha o suco ");  
+              delay(3000); 
+              digitalWrite(led_dis, LOW);
+              delay(2000);                      
+              if(ikey() != ' ') {
+                lcd.clear();
+                digitalWrite(led_dis, HIGH);
+                do{                            
+                  lcd.setCursor(0, 0);
+                  lcd.print("1 - Laranja");
+                  lcd.setCursor(0, 1);
+                  lcd.print("2 - Acerola"); 
+                  delay(2000);  
+                  lcd.clear();
+                  lcd.setCursor(0, 0);
+                  lcd.print("3 - Maracuja");
+                  lcd.setCursor(0, 1);
+                  lcd.print("4 - Abacaxi");
+                  delay(2000);
+                  lcd.clear();
+              }while (ikey() != ' '); 
+              }
+             // if(ikey() == '1')
                         
-            case 1:     lcd.clear();
-                        lcd.setCursor(0,0);
-                        lcd.print("1 - Laranja");
-                        lcd.setCursor(0,1);
-                        lcd.print("2 - Acerola"); 
-                        digitalWrite(bomba1, HIGH);
-                        digitalWrite(bomba2, LOW);
-                        break;
 
-            case 2:     lcd.clear();
-                        lcd.setCursor(0,0);
-                        lcd.print("3 - Uva");
-                        lcd.setCursor(0,1);
-                        lcd.print("4 - Morango");
-                        digitalWrite(bomba1, LOW);
-                        digitalWrite(bomba2, HIGH);
-                        break;  
-                                
-            case 3:     lcd.clear();
-                        lcd.setCursor(0,0);
-                        lcd.print("Suco 1 %:");
-                        lcd.setCursor(0,1);
-                        lcd.print("Digite:   ");
-                        digitalWrite(bomba1, HIGH);
-                        digitalWrite(bomba2, HIGH);
-                        break;  
-
-            case 4:     lcd.clear();
-                        lcd.setCursor(0,0);
-                        lcd.print("Suco 2 %:  ");
-                        lcd.setCursor(0,1);
-                        lcd.print("DIgite:   ");
-                        digitalWrite(bomba1, LOW);
-                        digitalWrite(bomba2, LOW);
-                        break;  
-
-            case 5:     lcd.clear();
-                        lcd.setCursor(0,0);
-                        lcd.print("Recipiente 1");
-                        lcd.setCursor(0,1);
-                        lcd.print("Acabando suco");
-                        digitalWrite(led1, HIGH);
-                        digitalWrite(led2, HIGH);
-                        digitalWrite(led3, HIGH);
-                        break;  
-
-            case 6:     lcd.clear();
-                        lcd.setCursor(0,0);
-                        lcd.print("Recipiente 2");
-                        lcd.setCursor(0,1);
-                        lcd.print("Sem suco");
-                        digitalWrite(led1, LOW);
-                        digitalWrite(led2, LOW);
-                        digitalWrite(led3, LOW);
-                        break;  
-                       
-    }
-
-    delay(2000);
-    menu++; 
-    if(menu == 7) menu = 0;
     
 }
-   
-   
-
